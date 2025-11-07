3fa0cbfb5bbdd632debe648dff552560dc14f6c1
udaykumar2507 <uk25072004@gmail.com>
2025-11-07 11:48:33 +0530

    PostgreSQL

---
+++ Week10/dist/index.js
@@ -0,0 +1,42 @@
+import { Client } from 'pg';
+const client = new Client({
+    connectionString: 'postgresql://postgres:1234@localhost:5432/postgres',
+});
+async function createTable() {
+    await client.connect().catch(err => console.error('Connection error', err.stack));
+    const query = `
+    CREATE TABLE IF NOT EXISTS users (
+      id SERIAL PRIMARY KEY,
+      name VARCHAR(100),
+      email VARCHAR(100) UNIQUE NOT NULL
+    );
+  `;
+    const res = await client.query(query);
+    console.log('Table created', res);
+}
+// createTable().finally(() => client.end());
+// Not a Better Practice to insert data like this
+// async  function insertTable() {
+//    await client.connect().catch(err => console.error('Connection error', err.stack));
+//    const query = `INSERT INTO  users (name,email) VALUES ('UDAY','uk25072004@gmail.com')`;
+//    const res = await client.query(query);
+//    console.log('Data inserted', res);
+// } 
+// insertTable().finally(() => client.end());
+async function InsertTable(name, email) {
+    await client.connect().catch(err => console.error('Connection error', err.stack));
+    const query = `INSERT INTO users (name, email) VALUES ($1, $2) RETURNING *`;
+    const values = [name, email];
+    const res = await client.query(query, values);
+    console.log('Data inserted', res.rows[0]);
+}
+// InsertTable('Ujval', 'Ujval@gmail.com').finally(() => client.end());  
+async function fetchUsers(email) {
+    await client.connect().catch(err => console.error('Connection error', err.stack));
+    const query = `SELECT * FROM users WHERE email = $1`;
+    const values = [email];
+    const res = await client.query(query, values);
+    console.log('User fetched', res.rows[0]);
+}
+fetchUsers('Ujval@gmail.com').finally(() => client.end());
+//# sourceMappingURL=index.js.map
