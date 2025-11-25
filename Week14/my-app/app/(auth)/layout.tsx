d184103f5029ec8a7a824bd07f0f79ecb3d4b27d
udaykumar2507 <uk25072004@gmail.com>
2025-11-25 21:28:20 +0530

    NextJs frontend

---
+++ Week14/my-app/app/(auth)/layout.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+export default function({children} : {
+    children : React.ReactNode
+    })  {  return (
+        <div>
+        <div className="border-b text-center">
+            20% off for the next 3 days
+        </div>
+        {children}
+        </div>
+    );
+    
+}
---
+++ Week14/my-app/app/(auth)/signin/page.tsx
@@ -0,0 +1,9 @@
+import { SignInComponent } from "@/app/components/SignIn";
+
+export default function Signin() {
+  return (
+    <div>
+      <SignInComponent />
+    </div>
+  );
+}
